4bf23fd558c0038e148eaf7fb8e15d72fa7499e4
Od1e <stopsniffingaxe@gmail.com>
2022-04-03 18:02:10 -0400

    add boilerplate code

---
+++ gv-park/src/views/ProfileView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <NavBar></NavBar>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from "vue-property-decorator";
+    import NavBar from "../components/NavBar.vue";
+
+    @Component({ components: { NavBar } })
+    export default class ProfileView extends Vue {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
