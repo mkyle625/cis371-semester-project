f509dfbf2c5a9e0b103891ede3a02eec2103d718
Kyle <40324912+mkyle625@users.noreply.github.com>
2022-04-03 18:31:17 -0400

    Show guest login page on profile if logged in as guest

--- gv-park/src/components/GuestLogin.vue
+++ gv-park/src/components/GuestLogin.vue
@@ -1,20 +1,134 @@
 <!-- Guest login page ONLY shown on the UserPage and SettingsPage when in guest mode -->
 
 <template>
-    <div id="GuestLoginComponent">
+    <div>
+        <div id="GuestLoginComponent">
+            <div class="logo">
+                <i class="fa-solid fa-road"></i>
+                <span>myGVPark</span>
+                <p>Please login to use this feature</p>
+            </div>
 
+            <div id="LoginBtn" @click="googleLogin">
+                <span class="btn">Login</span>
+            </div>
+
+            <i class="fa-solid fa-car-side fa-bounce" v-show="loading"></i>
+            <span id="errorBox" v-show="lError">{{loginError}}</span>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import {
+        Auth,
+        getAuth,
+        getRedirectResult,
+        GoogleAuthProvider,
+        signInWithPopup,
+        signInWithRedirect,
+        User,
+        UserCredential,
+    } from "firebase/auth";
 
 @Component
 export default class GuestLoginPage extends Vue {
-  
+    auth: Auth | null = null;
+
+        loginError = ""; // Holds error status text
+        lError = false; // Show error status text
+        loading = false; // Show loading indicator
+
+        mounted(): void {
+            this.auth = getAuth();
+        }
+
+        googleLogin(): void {
+            this.lError = false;
+            this.loading = true;
+            const provider = new GoogleAuthProvider();
+            // signInWithRedirect(this.auth!, provider);
+            // getRedirectResult(this.auth!)
+            // .then((result) => {
+            //     this.$router.push({name: "home"});
+            // })
+            // .catch((error) => {
+            //     this.loginError = error;
+            //     this.lError = true;
+            // })
+            signInWithPopup(this.auth!, provider)
+            .then((cred: UserCredential) => {
+                console.log(`Login successful: ${cred.user.displayName}`);
+                this.$router.push({ name: "home" });
+                this.loading = false;
+            })
+            .catch((err: any) => {
+                this.loginError = `Authentication error: ${err}`;
+                this.loading = false;
+                this.lError = true;
+            });
+        }
 }
 </script>
 
 <style scoped>
+    #GuestLoginComponent {
+        height: 100vh;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-color: #0065a4;
+    }
 
+    .btn {
+        border-radius: 20px;
+    }
+
+    #LoginBtn {
+        color: white;
+        margin-bottom: 5vh;
+    }
+
+    #LoginBtn > span {
+        font-size: 4vh;
+        padding: 2vh 10vw;
+        background-color: white;
+        color: #0065a4;
+    }
+
+    #GuestLoginBtn > span {
+        font-size: 2vh;
+        border: 1.5px solid white;
+        padding: 1.7vh 6vw;
+        color: white;
+    }
+
+    #errorBox {
+        margin-top: 5vh;
+        text-align: center;
+        color: rgb(255, 125, 125);
+    }
+
+    .fa-solid {
+        font-size: 4vh;
+        margin-bottom: 5px;
+        color: white;
+    }
+
+    .fa-car-side {
+        margin-top: 6vh;
+    }
+
+    .logo {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        color: white;
+        font-size: 2vh;
+        margin-top: 10vh;
+        padding-top: 10vh;
+        margin-bottom: 10vh;
+    }
 </style>
--- gv-park/src/views/ProfileView.vue
+++ gv-park/src/views/ProfileView.vue
@@ -1,16 +1,18 @@
-<template>
+<template v-if="loginType === 'guest'">
     <div>
         <NavBar></NavBar>
+        <GuestLogin></GuestLogin>
     </div>
 </template>
 
 <script lang="ts">
     import { Component, Vue } from "vue-property-decorator";
     import NavBar from "../components/NavBar.vue";
+    import GuestLogin from "../components/GuestLogin.vue";
 
-    @Component({ components: { NavBar } })
+    @Component({ components: { NavBar, GuestLogin } })
     export default class ProfileView extends Vue {
-
+        
     }
 </script>
 
