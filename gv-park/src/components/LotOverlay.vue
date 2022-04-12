d0eff861541e341a302de78385b93a6499776ee2
Kyle <40324912+mkyle625@users.noreply.github.com>
2022-04-12 10:23:07 -0400

    Added template for lot type/can't park here

--- gv-park/src/components/LotOverlay.vue
+++ gv-park/src/components/LotOverlay.vue
@@ -4,6 +4,11 @@
             <span>Lot {{lot}}</span>
             <i class="fa-solid fa-circle-xmark" @click="closeOverlay"></i>
         </div>
+        <div id="type">
+            <span>Lot type here - </span>
+            <i class="fa-solid fa-exclamation-triangle"></i>
+            <p>You can't park here with your pass!</p>
+        </div>
     </div>
 </template>
 
@@ -38,12 +43,29 @@
         flex-direction: row;
         justify-content: space-between;
     }
-
+    
     #header > span {
         margin-left: 3vw;
         margin-top: 1.5vh;
         font-size: 2em;
         font-weight: bold;
+    }
+
+    #type {
+        display: flex;
+        flex-direction: row;
+    }
+
+    #type > span {
+        margin-left: 3vw;
+        margin-top: 1.5vh;
+        font-style: italic;
+    }
+
+    .fa-exclamation-triangle {
+        color: red;
+        font-size : 1em;
+        margin-top: 1vw;
     }
 
     .fa-circle-xmark {
