5292c8686b4e7338f40516043effbe58e4bcda34
ummayair ahmad <ummay35@icloud.com>
2022-04-12 01:06:15 -0400

    WIP - creating picker to change parkingPassType

--- gv-park/src/views/SettingsView.vue
+++ gv-park/src/views/SettingsView.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
-      <NavBar></NavBar>
-      <GuestLogin v-if="this.$store.state.isGuest === true"></GuestLogin>
-      <div v-else class = settingsViewContainer>
-        <div class="LogoutBtn" @click="logoutFirebase">
+    <NavBar></NavBar>
+    <GuestLogin v-if="this.$store.state.isGuest === true"></GuestLogin>
+    <div v-else class="settingsViewContainer">
+      <form>
+        <label for="parkingPassType">Parking pass type:</label><br />
+        <select id="parkingPassType">
+          <option value="studentCommuterPass">Student Commuter pass</option>
+          <option value="lotJCommuterPass">Lot J Remote Commuter</option>
+          <option value="allendaleStudentPass">
+            Allendale Student Resident
+          </option>
+          <option value="calderStudentPass">Calder Student Resident</option>
+          <option value="pewStudentPass">Pew Student Resident</option>
+          <option value="remoteResidentSewardPass">
+            Remote Resident Seward Ramp
+          </option>
+        </select>
+      </form>
+
+      <div class="LogoutBtn" @click="logoutFirebase">
         <span>Logout</span>
-        </div>
       </div>
+    </div>
   </div>
 </template>
 
@@ -20,6 +36,9 @@ import { Auth, getAuth, signOut } from "firebase/auth";
 export default class SettingsView extends Vue {
   auth: Auth | null = null;
   mounted(): void {
+    var e = document.getElementById("parkingPassType");
+    var value = e.options[e.selectedIndex].value;
+    var text = e.options[e.selectedIndex].text;
     console.log("Logged in as guest: " + this.$store.state.isGuest);
   }
 
@@ -35,18 +54,18 @@ export default class SettingsView extends Vue {
 
 <style scoped>
 .LogoutBtn {
-        color: #0065a4;
-        margin-bottom: 1vh;
-        margin-top: 8vh;
-        align-content: center;
-    }
-    .settingsViewContainer {
-    height: 100vh;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    font-size: 2vh;
-    padding-top: 2vh;
+  color: #0065a4;
+  margin-bottom: 1vh;
+  margin-top: 8vh;
+  align-content: center;
+}
+.settingsViewContainer {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 2vh;
+  padding-top: 2vh;
 }
 </style>
