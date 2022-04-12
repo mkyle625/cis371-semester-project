ccb0b5a1fd88dea880248103638409fd662ea993
ummayair ahmad <ahmadu@mail.gvsu.edu>
2022-04-12 17:26:55 -0400

    user can now update parkingPassType in settings

--- gv-park/src/views/SettingsView.vue
+++ gv-park/src/views/SettingsView.vue
@@ -4,21 +4,24 @@
     <GuestLogin v-if="this.$store.state.isGuest === true"></GuestLogin>
     <div v-else class="settingsViewContainer">
       <form>
-        <label for="parkingPassType">Parking pass type:</label><br />
-        <select id="parkingPassType">
-          <option value="studentCommuterPass">Student Commuter pass</option>
-          <option value="lotJCommuterPass">Lot J Remote Commuter</option>
-          <option value="allendaleStudentPass">
-            Allendale Student Resident
-          </option>
-          <option value="calderStudentPass">Calder Student Resident</option>
-          <option value="pewStudentPass">Pew Student Resident</option>
-          <option value="remoteResidentSewardPass">
-            Remote Resident Seward Ramp
-          </option>
-        </select>
-      </form>
-
+          <label for="parkingPassType">Parking pass type:</label><br />
+          <select id="parkingPassType" v-model="selectedPassType">
+            <option disabled=true>Please choose one</option>
+            <option value="Student Commuter Pass">Student Commuter pass</option>
+            <option value="Lot J Remote Commuter Pass">Lot J Remote Commuter</option>
+            <option value="Allendale Student Resident Pass">
+              Allendale Student Resident
+            </option>
+            <option value="Calder Student Resident Pass">Calder Student Resident</option>
+            <option value="Pew Student Resident Pass">Pew Student Resident</option>
+            <option value="Remote Resident Seward Ramp Pass">
+              Remote Resident Seward Ramp
+            </option>
+          </select>
+        </form>
+    <button @click="saveToFirebase">save</button>
+    <span>{{selectedPassType}}</span>
+    
       <div class="LogoutBtn" @click="logoutFirebase">
         <span>Logout</span>
       </div>
@@ -31,21 +34,33 @@ import { Component, Vue } from "vue-property-decorator";
 import NavBar from "../components/NavBar.vue";
 import GuestLogin from "../components/GuestLogin.vue";
 import { Auth, getAuth, signOut } from "firebase/auth";
+import { db } from "../myconfig";
 
 @Component({ components: { NavBar, GuestLogin } })
 export default class SettingsView extends Vue {
   auth: Auth | null = null;
+  selectedPassType = "";
+
   mounted(): void {
-   
     console.log("Logged in as guest: " + this.$store.state.isGuest);
   }
 
   logoutFirebase(): void {
-    console.log("clicked");
+   console.log("clickedLogout");
     this.auth = getAuth();
     signOut(this.auth);
     this.$store.state.guestLogin = false;
     this.$router.back();
+  }
+  async saveToFirebase():Promise<void>{
+    console.log("clicked");
+    const auth = getAuth();
+    const userId = auth.currentUser?.uid;
+
+    const collectionRef = db.collection("USERS").doc(userId).collection("Profile Info").doc("ProfileView");
+    const parkingPassFieldRef = await collectionRef.update({parkingPassType:this.selectedPassType}).then(()=>{
+      console.log("Saved !!!")
+    });
   }
 }
 </script>
@@ -54,7 +69,7 @@ export default class SettingsView extends Vue {
 .LogoutBtn {
   color: #0065a4;
   margin-bottom: 1vh;
-  margin-top: 8vh;
+  margin-top: 15vh;
   align-content: center;
 }
 .settingsViewContainer {
