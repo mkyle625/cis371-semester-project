2423f5d85afc6e8b8e2bb742334590da8cecf8bd
Kyle <40324912+mkyle625@users.noreply.github.com>
2022-04-19 10:25:18 -0400

    Added weather component, dummy code for now

---
+++ gv-park/src/components/Weather.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="weather">
+        
+    </div>
+</template>
+
+<script lang="ts">
+    import { Vue, Component, Prop } from "vue-property-decorator";
+
+    @Component
+    export default class Weather extends Vue {
+        
+    }
+</script>
+
+<style scoped>
+    
+</style>
