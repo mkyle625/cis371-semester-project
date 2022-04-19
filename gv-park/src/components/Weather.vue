7c173a0405caac5c579730bfeedf3c84ab323835
Kyle <40324912+mkyle625@users.noreply.github.com>
2022-04-19 10:32:36 -0400

    Added Axios (use npm install), basic weather response

--- gv-park/src/components/Weather.vue
+++ gv-park/src/components/Weather.vue
@@ -6,9 +6,19 @@
 
 <script lang="ts">
     import { Vue, Component, Prop } from "vue-property-decorator";
+    import axios, { AxiosResponse } from "axios";
 
     @Component
     export default class Weather extends Vue {
+        mounted() {
+            axios.request({
+                url: "https://api.openweathermap.org/data/2.5/weather?lat=42.959790595367124&lon=-85.8891659975052&appid=7481c0a9c63f8d54edc0ba3dc45ebcca",
+                method: "GET",
+                responseType: "json"
+            }).then((response: AxiosResponse) => {
+                console.log(response.data);
+            });
+        }
         
     }
 </script>
